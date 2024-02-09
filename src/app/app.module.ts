153e5469fbd4efa97d851da0529a74d8b28f28d7
Ivanncho <jovanovski.ivanco@yahoo.com>
2024-02-09 20:08:59 +0100

    Adding toolbar and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,16 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsHeaderComponent
   ],
   imports: [
     BrowserModule,
