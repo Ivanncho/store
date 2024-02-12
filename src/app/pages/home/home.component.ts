73622c6c3ed5b799054d715a9c2490cee197edb8
Ivanncho <jovanovski.ivanco@yahoo.com>
2024-02-12 15:43:57 +0100

    Adding product card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT : {[id: number]: number} = {
+  1: 400,
+  3: 335,
+  4: 350
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   cols = 3;
   category: string | undefined;
+  rowHeight = ROWS_HEIGHT[this.cols];
 
   constructor() { }
 
@@ -15,6 +22,7 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNum:number):void{
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
   onShowCategory(newCategory: string): void{
     this.category = newCategory;
