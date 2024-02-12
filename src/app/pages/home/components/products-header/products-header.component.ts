3fcf20e2a04f76adfbaf7f884cac1c181b4e9d4e
Ivanncho <jovanovski.ivanco@yahoo.com>
2024-02-12 14:58:16 +0100

    Added sorting buttons

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
   templateUrl:'products-header.component.html'
 })
 export class ProductsHeaderComponent implements OnInit {
+  @Output() columnsCountChange = new EventEmitter<number>();
+
   sort = 'desc';
+  itemsShowCount = 12;
+
 
   constructor() { }
 
@@ -14,5 +18,12 @@ export class ProductsHeaderComponent implements OnInit {
 
   onSortUpdated(newSort: string): void{
     this.sort = newSort;
+  }
+  onItemsUpdated(count: number):void{
+    this.itemsShowCount = count;
+  }
+
+  onColumnsUpdated(colsNum: number):void{
+    this.columnsCountChange.emit(colsNum);
   }
 }
