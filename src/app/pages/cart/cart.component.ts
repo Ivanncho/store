a9dfb14830ad5a181a7936e82890c292823ed0a0
Ivanncho <jovanovski.ivanco@yahoo.com>
2024-02-13 14:44:11 +0100

    Added all of the cart features

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart, CartItem } from 'src/app/Models/cart';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,36 +8,38 @@ import { Cart, CartItem } from 'src/app/Models/cart';
 })
 export class CartComponent implements OnInit {
 
-  cart: Cart = {items: [{
-    product: 'https://via.placeholder.com/150',
-    name: "Nike Air Max 90",
-    price: 150,
-    quantity: 1,
-    id: 1
-  },
-  {
-    product: 'https://via.placeholder.com/150',
-    name: "Nike Air Force 1",
-    price: 150,
-    quantity: 3,
-    id: 2
-  },
-  {
-    product: 'https://via.placeholder.com/150',
-    name: "Adidas Samba",
-    price: 150,
-    quantity: 1,
-    id: 3
-  },
-  {
-    product: 'https://via.placeholder.com/150',
-    name: "New Balance 470",
-    price: 150,
-    quantity: 2,
-    id: 4
-  },]};
+  cart: Cart = {
+    items: [{
+      product: 'https://via.placeholder.com/150',
+      name: "Nike Air Max 90",
+      price: 150,
+      quantity: 1,
+      id: 1
+    },
+    {
+      product: 'https://via.placeholder.com/150',
+      name: "Nike Air Force 1",
+      price: 150,
+      quantity: 3,
+      id: 2
+    },
+    {
+      product: 'https://via.placeholder.com/150',
+      name: "Adidas Samba",
+      price: 150,
+      quantity: 1,
+      id: 3
+    },
+    {
+      product: 'https://via.placeholder.com/150',
+      name: "New Balance 470",
+      price: 150,
+      quantity: 2,
+      id: 4
+    },]
+  };
 
-  dataSource: Array<CartItem> =[];
+  dataSource: Array<CartItem> = [];
   displayedColumns: Array<string> = [
     'product',
     'name',
@@ -45,16 +48,33 @@ export class CartComponent implements OnInit {
     'total',
     'action'
   ];
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
-    this.dataSource = this.cart.items
+    this.dataSource = this.cart.items;
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    })
   }
 
-  getTotal(items: Array<CartItem>): number{
-    return items
-    .map((item) => item.price * item.quantity)
-    .reduce((prev, current) => prev + current , 0);
+  getTotal(items: Array<CartItem>): number {
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveFromCartItem(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+
+  onAddQuantity(item: CartItem): void{
+    this.cartService.addToCart(item);
+  }
+  onRemoveQuantity(item: CartItem): void{
+    this.cartService.removeQuantity(item);
   }
 
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -21,8 +21,36 @@ export class CartService {
       items.push(item);
     }
     this.cart.next({ items });
-    this._snackBar.open(`${item.name} added to the cart`, "Ok", {duration: 2000});
+    this._snackBar.open(`${item.name} added to the cart`, "Ok", { duration: 2000 });
     console.log(this.cart.value);
+  }
+  getTotal(items: Array<CartItem>): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
+  clearCart(): void {
+    this.cart.next({ items: [] });
+    this._snackBar.open("Cart is clear", "Ok", { duration: 2000 });
+  }
+  removeFromCart(item: CartItem): void {
+    const filteredItems = this.cart.value.items.filter((_item) => {
+      _item.id !== item.id
+    })
+    this.cart.next({ items: filteredItems });
+    this._snackBar.open(`${item.name} removed from cart`, 'Ok', { duration: 2000 });
+  }
+  removeQuantity(item: CartItem): void {
+    this.cart.value.items
+      .map((_item) => {
+        if (_item.id === item.id) {
+          _item.quantity--;
+          this._snackBar.open(`1 ${item.name} is removed from cart.`, 'Ok', { duration: 2000 });
+          if (_item.quantity === 0) {
+            this.removeFromCart(_item);
+          }
+        }
+      })
   }
 
 }
